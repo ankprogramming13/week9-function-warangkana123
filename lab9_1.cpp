0d501ac5be879902fd76022bcd6719e65a9e5601
Unknown <warangkanacake.41@gmail.com>
2018-01-19 13:50:08 +0700

    ข้อที่ 1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,12 +2,77 @@
 
 using namespace std;
 
+void swapNumber (int *x, int *y) {
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void showValue (int x, int y) {
+    cout << "This is x : " << x << endl;
+    cout << "this is y : " << y << endl;
+}
+
+int main() {
+    /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
+    โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
+    และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
+    */#include <iostream>
+
+using namespace std;
+
+void swapNumber (int *x, int *y) {
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void showValue (int x, int y) {
+    cout << "This is x : " << x << endl;
+    cout << "this is y : " << y << endl;
+}
+
 int main() {
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
     */
+    int a,b;
+    cin >> a;
+    cin >> b;
+    showValue(a, b);
 
+    int cmd;
+
+    do {
+        cin >> cmd;
+        if (cmd == 1) {
+            swapNumber(&a, &b);
+            showValue(a, b);
+
+        }
+    } while (cmd != 2);
+
+
+    return 0;
+}
+    int a,b;
+    cin >> a;
+    cin >> b;
+    showValue(a, b);
+
+    int cmd;
+
+    do {
+        cin >> cmd;
+        if (cmd == 1) {
+            swapNumber(&a, &b);
+            showValue(a, b);
+
+        }
+    } while (cmd != 2);
 
 
     return 0;
